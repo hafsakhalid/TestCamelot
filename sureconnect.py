6e08be7de7bd5149a32b174776919132364624ae
Hafsa Khalid <hafsa.khalid@mail.mcgill.ca>
2021-01-28 16:27:27 -0500

    updated sureconnect

--- sureconnect.py
+++ sureconnect.py
@@ -11,7 +11,7 @@ import sys
 import pdb
 import doctest
 from sys import argv
-#import logging
+#importlogging
 
 from pandera.typing import Series
 import pandera as pa
@@ -27,13 +27,13 @@ import rank_din_fix
 # First parameter is the config file for this MGA.
 config_filename = sys.argv[1]
 
-# Second parameter is the current PDF we're parsing.
+# # Second parameter is the current PDF we're parsing.
 pdf_file = sys.argv[2]
 
-# Third parameter is the page of the PDF to parse.
+# # Third parameter is the page of the PDF to parse.
 page_number = sys.argv[3]
 
-# Fourth parameter is the template to populate.
+# # Fourth parameter is the template to populate.
 template_file = sys.argv[4]
 
 # TODO: loading config file w yaml
@@ -42,8 +42,8 @@ import config
 # config = yaml.safe_load(open(config_filename))
 # print(config.string_match["title_name"])
 
-pdf = pdf_file
-pagelist = page_number
+tables = camelot.read_pdf( pdf_file, pages=page_number, flavor=config.extracts['flavour'])
+#df = tables[0].df
 
 #def mock_read_pdf(pdf_filename, pages, flavor):
 #    from types import SimpleNamespace
@@ -97,142 +97,141 @@ pagelist = page_number
 #pdb.set_trace()
 #df = extract_tables(pdf_file, page_number, config.extracts['flavour'], camelot.read_pdf)
 
-tables = camelot.read_pdf(pdf_file, page_number, config.extracts['flavour'])
-df = tables[0].df
+#df = tables[0].df
 #print(df)
 
-#if __name__ == "__main__":
-#    import sys
-#    import doctest
-#
-#    # Return the number of doctests that fail. This gives calling scripts
-#    # a non-zero exit code if there are failing tests.
-#    sys.exit(doctest.testmod(verbose=True)[0])
-#
-#sys.exit(0)
-#
 # get standard columns for each df,
 # string for 0 - num of columns
 
-def standardcols(df):
-    # df.columns = df.columns.apply(str)
-    cols = list(df.columns)
-    for i in cols:
-        cols[i] = str(i)
-        df.columns = cols
-    return df
 
-# Standardize columns for stitching.
-for table in tables:
-    tables[table].df = standardcols(tables[table].df)
-
-sys.exit(0)
-
-# then you define your schema:
-schema = pa.DataFrameSchema(
-    {
-        "0": pa.Column(
-            str,
-            checks=[
-                # define custom checks as functions that take a series as input and
-                pa.Check(lambda s: s.str.split("\n", expand=True).shape[0] == 1)
-            ],
-        )
-    }
-)
-# conditions keeps track of which fix_up has been applied
-
-fix_up = dict()
-fix_up["SchemaErrors"] = rank_din_fix.rank_din_fix
-
-conditions = {e: True for e in fix_up}
-while all(conditions):
-    try:
-        schema.validate(df, lazy=True)
-    except Exception as exception:
-        error_name = type(exception).__name__  # errorheadname
-    if not conditions["SchemaErrors"]:
-        logging.info("Already tried fix up!")
-        break
-    else:
-        logging.info("Trying fix up")
-        conditions["SchemaErrors"] = fix_up["SchemaErrors"](df)
-        break  # only here because no other fix ups in the list yet
-
-# updating the df
-df = conditions["SchemaErrors"]
-# this is going to return the dataFrame after tryng all the fix_ups
+#def standardcols(dataFrame=df):
+    
+    # """ Should return df columns as a string 
 
 
-# ideally all this will already be in a DataFrame because we called sureconnect on the first page before
-table = camelot.read_pdf("2020.pdf", pages="1", flavor="stream")
-df1 = table[0].df
+    # >>> standardcols() # doctest: +NORMALIZE_WHITESPACE
+    #          0                                       1                                      2   3            4            5
+    # 0                               Period Covered                                                                     
+    # 1                                                                                      To  30-Jun-2019  30-Jun-2020
+    # 2                   Average Number of Insureds                                                      86           82
+    # 3   A)                           Paid Premiums                                                $258,497     $230,125
+    # 4                            Adjusted Premiums                                                $241,033     $230,125
+    # 5                (excludes plan change impact)                                                                     
+    # 6                             Pooling Premiums                                               $(42,864)    $(40,924)
+    # 7       Quebec Drug Insurance Pooling Premiums                                                       –            –
+    # 8   B)            Non Pooled Adjusted Premiums                                                $198,169     $189,201
+    # 9               Paid Claims (Net of Cost Plus)                                                $135,098     $139,511
+    # 10                      Normalized Paid Claims                                                $135,098     $150,672
+    # 11                              Pooling Credit                                                  $(261)       $(220)
+    # 12        Quebec Drug Insurance Pooling Credit                                                       –            –
+    # 13                      Change in IBNR Reserve                                                $(2,336)         $356
+    # 14  C)              Non Pooled Incurred Claims                                                $132,501     $150,808
+    # 15  D)                              Loss Ratio                                                   66.9%        79.7%
+    # 16  E)                                   Trend                                                   29.0%        15.7%
+    # 17  F)                        Retention Factor                                                   22.4%        22.4%
+    # 18  G)                     Adjusted Loss Ratio                                                  111.2%       118.8%
+    # 19  H)                               Weighting                                                   25.0%        75.0%
+    # 20                                                                Weighted Loss Ratio                        116.9%
+    # 21                                                                        Credibility                        100.0%
+    # 22                                              Adjusted Loss Ratio after Credibility                        116.9%
+    # 23                                                                   Final Loss Ratio                        117.0%
+    # 24                                                                    Rate Adjustment                           17%
+    # 25                                                              Final Rate Adjustment                           17%
+     
+    # """
 
-df1 = df1.drop([0, 1], axis=0)
-
-# make the columns the same as the scehma
-df.columns = df1.columns
-df1 = df1.append(df, ignore_index=True)
 
 
-def fxn(col):
-    return " ".join(col.to_string(index=False).split())
+#Standardize columns for stitching, by changing the column headers to string, so even before getting the first table, each column name is a string
+for table in tables: 
+    table.df.columns = table.df.columns.astype(str)
 
 
-cols = df1[:3].apply(fxn, axis=0)
+#then you define your schema for each pdf, this should take in a tableList 
+def define_schema(tableList=tables):
+    # """ Define a schema for each table in the PDF and see if it passes or fails 
+    
+    # >>> define_schema(tables[0].df)
+    # Trying fix_up
+    #      0                                       1                                      2   3            4            5
+    # 0                               Period Covered                                                                     
+    # 1                                                                                      To  30-Jun-2019  30-Jun-2020
+    # 2                   Average Number of Insureds                                                      86           82
+    # 3   A)                           Paid Premiums                                                $258,497     $230,125
+    # 4                            Adjusted Premiums                                                $241,033     $230,125
+    # 5                (excludes plan change impact)                                                                     
+    # 6                             Pooling Premiums                                               $(42,864)    $(40,924)
+    # 7       Quebec Drug Insurance Pooling Premiums                                                       –            –
+    # 8   B)            Non Pooled Adjusted Premiums                                                $198,169     $189,201
+    # 9               Paid Claims (Net of Cost Plus)                                                $135,098     $139,511
+    # 10                      Normalized Paid Claims                                                $135,098     $150,672
+    # 11                              Pooling Credit                                                  $(261)       $(220)
+    # 12        Quebec Drug Insurance Pooling Credit                                                       –            –
+    # 13                      Change in IBNR Reserve                                                $(2,336)         $356
+    # 14  C)              Non Pooled Incurred Claims                                                $132,501     $150,808
+    # 15  D)                              Loss Ratio                                                   66.9%        79.7%
+    # 16  E)                                   Trend                                                   29.0%        15.7%
+    # 17  F)                        Retention Factor                                                   22.4%        22.4%
+    # 18  G)                     Adjusted Loss Ratio                                                  111.2%       118.8%
+    # 19  H)                               Weighting                                                   25.0%        75.0%
+    # 20                                                                Weighted Loss Ratio                        116.9%
+    # 21                                                                        Credibility                        100.0%
+    # 22                                              Adjusted Loss Ratio after Credibility                        116.9%
+    # 23                                                                   Final Loss Ratio                        117.0%
+    # 24                                                                    Rate Adjustment                           17%
+    # 25                                                              Final Rate Adjustment                           17%
+    
+    
+    # """
+    schema = pa.DataFrameSchema({
+        '0':pa.Column(
+            pa.String, 
+            checks = [
+                pa.Check(lambda s : s.str.contains('\n') == True)
+            ]),    
+        })
+    #define the condtions dicitonary as True
+    fix_up = dict()
+    fix_up['0'] = 10 #arbritary value for now
+    conditions = {e : True for e in fix_up}
+    
+    while(all(conditions)):
+        try:
+            schema.validate(tableList, lazy=True)
+        except pa.errors.SchemaErrors as errors:
+            check_number = str(errors.failure_cases['column'][0]) 
+            #this is the column number that fails the check, need this as a key, so change to str
+            if(not conditions[check_number]): 
+                print("Already tried fix_up")
+                break
+            else: 
+                print("Trying fix_up")
+                conditions[check_number] = fix_up[check_number]#(tableList)
+                break #ask Alex why this does not quit the while loop
+    
+    return tableList
 
-df2 = df1[3:].reset_index(drop=True)
-df2.columns = list(cols)  # don't know if need "list"
+#this applies the schema to all the tables in the tablelist, an outputs a dataFrame
+final_df = pd.DataFrame()
+for table in tables: 
+    final_df = final_df.append(define_schema(table.df), ignore_index=True)
 
-print(df2)
+
 
 # Writing to a template
 # template should the argument but it does not accept a variable, so ask Alex
-template = load_workbook(
-    "Renewal Template Proof Zero.xlsm", read_only=False, keep_vba=True
-)
-writer = pd.ExcelWriter("Renewal Template Proof Zero.xlsm", engine="openpyxl")
-writer.book = template
-writer.sheets = {ws.title: ws for ws in template.worksheets}
-# for sheet in writer.sheets:
-# print(sheet)
 
-df2.to_excel(writer, sheet_name="Accounts", startrow=1, index=False)
-writer.save()
+def output_template(df=final_df, template=template_file, engine="openpyxl", lib=pd.ExcelWriter): 
+    #should import loadworkbook
+    output = load_workbook (
+        "Renewal Template Proof Zero.xlsm", read_only=False, keep_vba=True
+    )
+    writer = pd.ExcelWriter("Renewal Template Proof Zero.xlsm", engine=engine)
+    writer.book = output
+    writer.sheets = {ws.title: ws for ws in template.worksheets}
+    final_df.to_excel(writer, sheet_name="Accounts", startrow=1, index=False)
+    writer.save()
 
-
-# brute force the files into excel
-table = camelot.read_pdf(
-    "2020-ABC Client-Drug Report-Jul-Jun Proof Zero.pdf",
-    pages="1",
-    flavor="stream",
-)
-df3 = table[0].df
-template = load_workbook(
-    "Renewal Template Proof Zero.xlsm", read_only=False, keep_vba=True
-)
-writer = pd.ExcelWriter("Renewal Template Proof Zero.xlsm", engine="openpyxl")
-writer.book = template
-writer.sheets = {ws.title: ws for ws in template.worksheets}
-df3.to_excel(writer, sheet_name="2020-ABC", startrow=1, index=False)
-writer.save()
-
-table = camelot.read_pdf(
-    "Renewal Example Proof Zero Edited 12_14.pdf", pages="16", flavor="stream"
-)
-df4 = table[0].df
-template = load_workbook(
-    "Renewal Template Proof Zero.xlsm", read_only=False, keep_vba=True
-)
-writer = pd.ExcelWriter("Renewal Template Proof Zero.xlsm", engine="openpyxl")
-writer.book = template
-writer.sheets = {ws.title: ws for ws in template.worksheets}
-df4.to_excel(writer, sheet_name="By Claims", startrow=1, index=False)
-
-writer.save()
-
-
-# try it with 16 and 15 (comes out clean)
 
 if __name__ == "__main__":
     import sys
@@ -241,3 +240,5 @@ if __name__ == "__main__":
     # Return the number of doctests that fail. This gives calling scripts
     # a non-zero exit code if there are failing tests.
     sys.exit(doctest.testmod(verbose=True)[0])
+
+sys.exit(0)
