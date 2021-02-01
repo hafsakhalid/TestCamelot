2d6aa271be7bb172872c1e098823b7ffa648801f
Alexander Flanagan <alex.flanagan@gmail.com>
2021-02-01 09:51:49 -0500

    Remove comments and old import. Add no_fix lambda that returns the input dataframe

--- functionmap.py
+++ functionmap.py
@@ -1,16 +1,10 @@
 import rank_din_fix
-import no_fix
-
 
 def contains_new_line(df):
     return df.applymap(lambda x: x.find('\n') > 0)
-    # return s.str.contains('\n')
-    # # if(s.find('\n') == -1): 
-    # #     return False
-    # # return True
 
 fxn_map = {
     'contains_new_line' : contains_new_line, 
-    'rank_din_fix' : rank_din_fix.rank_din_fix
-    #need to include a no fix up 
+    'rank_din_fix' : rank_din_fix.rank_din_fix,
+    'no_fix' : (lambda df: df)
 }
