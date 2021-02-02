73708565d55dd568531eacac5adbeac23f4a7fb6
Alexander Flanagan <alex.flanagan@gmail.com>
2021-02-02 08:16:31 -0500

    Removed talking

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Beginning data extraction and template mapping."
-say "Beginning data extraction and template mapping." &
+# say "Beginning data extraction and template mapping." &
 cp output_template.xlsx output.xlsx
 ./workflow.sh config.yaml 2020.pdf Renewal\ Template\ Proof\ Zero.xlsm
 ./workflow.sh config.yaml 2020-ABC\ Client-Drug\ Report-Jul-Jun\ Proof\ Zero.pdf Renewal\ Template\ Proof\ Zero.xlsm
 ./workflow.sh config.yaml Renewal\ Example\ Proof\ Zero\ Edited\ 12_14.pdf Renewal\ Template\ Proof\ Zero.xlsm
 echo "PDF data extraction and template mapping complete."
-say "PDF data extraction and template mapping complete. Extracted three tables to Excel." &
+# say "PDF data extraction and template mapping complete. Extracted three tables to Excel." &
 open output.xlsx
